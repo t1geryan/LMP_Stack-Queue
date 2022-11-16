3c046461cd66f9d4a0ccc54a18ae476275f9bbab
t1geryan <sgta1980@gmail.com>
2022-11-16 23:33:00 +0300

    solve family tree task

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 #include "ListStack.h"
+#include "ListQueue.h"
 #include <crtdbg.h>
 #include <string>
-
+#include <fstream>
 
 bool check_bracket_balance(std::string str) 
 {
@@ -26,9 +27,53 @@ bool check_bracket_balance(std::string str)
 	return result && stack.isEmpty();
 }
 
+bool print_all_childs(int ** matrix, int size, int humanI)
+{
+	bool isHaveChilds = false;
+	Queue qu;
+	for (int i = 0; i < size; ++i) // сначала закладываем всех прямых потомков в очередь
+		if (matrix[humanI][i]) {
+			isHaveChilds = true;
+			qu.push(i);
+		}
+	while (!qu.isEmpty()) { // если прямые потомки есть то выводим их индексы добавляем их прямых потомков
+		int index = qu.get_head();
+		qu.pop();
+		std::cout << index << ' ';
+		for (int i = 0; i < size; ++i)
+			if (matrix[index][i])
+				qu.push(i);
+	}
+	return isHaveChilds;
+}
+
 int main()
 {
-	std::cout << check_bracket_balance("(a+[c*{a+b}+d]-2)");
+	std::ifstream file("family_tree.txt");
+	if (file) {
+		int n;
+		file >> n;
+		int** matrix = new int* [n];
+		for (int i = 0; i < n; ++i)
+			matrix[i] = new int[n];
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				file >> matrix[i][j];
+				std::cout << matrix[i][j] << ' ';
+			}
+			std::cout << '\n';
+		}
+		std::cout << '\n';
+
+
+		if (!print_all_childs(matrix, n, 2)) 
+			std::cout << "Don't have childs!\n";
+
+		for (int i = 0; i < n; ++i)
+			delete[] matrix[i];
+		delete[] matrix;
+	}
 	std::cin.get();
 	_CrtDumpMemoryLeaks();
 }
